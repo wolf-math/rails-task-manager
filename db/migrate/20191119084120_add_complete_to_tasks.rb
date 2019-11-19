e102f782bb5be8e57bc796f59d3d9a8e652c0afd
Aaron Wolf <aaron.wolf@gmail.com>
2019-11-19 21:59:10 +0200

    added CRUD functionality

---
+++ db/migrate/20191119084120_add_complete_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddCompleteToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :completed, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_095424) do
+ActiveRecord::Schema.define(version: 2019_11_19_084120) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
   end
 
 end
